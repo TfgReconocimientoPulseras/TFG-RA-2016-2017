e1102ce681a72cc993e958050f7894042c512491
Apidal <alex.pidal@hotmail.com>
2016-10-26 00:31:20 +0200

    Añadido que muestre por pantalla que se ha enviado le fichero, que ponga nombre por defecto al fichero si no se ha puesto nombre y añadido espacio en los botones para separarlo del input

--- webapp/guardaDatos.php
+++ webapp/guardaDatos.php
@@ -2,7 +2,15 @@
 	if(isset($_POST['myData']) && isset($_POST['filename'])){
 	 	$obj = json_decode($_POST['myData']);
 
-	 	$myfile = fopen($_POST['filename'].".csv", "w") or die("Unable to open file!");
+	 	
+
+	 	if($_POST['filename'] == NULL)
+	 		$nombre = date("m,d,y G,i,s");
+	 	else
+	 		$nombre = $_POST['filename'];
+	 	
+
+	 	$myfile = fopen($nombre.".csv", "w") or die("Unable to open file!");
 
 	    foreach($obj as $o)
 	    {
