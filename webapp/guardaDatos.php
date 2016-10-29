209cc621dcf0a43522ff3d364e407178f0ab57ca
ivanClass <ivan.aguilera103@gmail.com>
2016-10-29 14:50:00 +0200

    Aplicación recogida datos: -Añadido tiempo (barra progreso). -Interfaz mejorada.

--- webapp/guardaDatos.php
+++ webapp/guardaDatos.php
@@ -1,5 +1,6 @@
 <?php
 	if(isset($_POST['myData']) && isset($_POST['nombreUsu']) && isset($_POST['tiempoGrabacion']) && isset($_POST['actividad'])){
+
 	 	$obj = json_decode($_POST['myData']);
 
 	 	
@@ -10,8 +11,9 @@
 		$nombre = $usu . "_" . $actividad . "_" . $tiempo . "_" . date("m-d-Y G;i;s");
 	 	
 
-	 	$myfile = fopen($nombre.".csv", "w") or die("Unable to open file!");
-
+	 	$myfile = fopen("datos/".$nombre.".csv", "w") or die("Unable to open file!");
+	 	fwrite($myfile, 'sep=;'.PHP_EOL);
+	 	fwrite($myfile, 'timestamp;gyro-alpha;gyro-beta;gyro-gamma;accel-x;accel-y;accel-z'.PHP_EOL);
 	    foreach($obj as $o)
 	    {
 	        $text = $o->ts.';'.$o->ga.';'.$o->gb.';'.$o->gg.';'.$o->ax.';'.$o->ay.';'.$o->az.PHP_EOL;
